12ada9bbe777694067e13a444b8f24e37ce71954
KeroppiKun <fuga0809ace@gmail.com>
2024-05-22 08:26:48 +0900

    test

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -10,7 +10,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor(): #test
 		velocity.y += gravity * delta
 
 	# Handle jump.
